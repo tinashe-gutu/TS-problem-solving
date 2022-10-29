5b0593fb3bde67217e0fcb1ac5bd906d75b1f16c
Tinashe Gutu <tinashegutu2011@gmail.com>
2022-10-29 19:03:48 +0100

    change files to TypeScript file

---
+++ src/katas/7kyu/countXsAndOs.ts
@@ -0,0 +1,20 @@
+
+/* Brief: check if number of 'x' and 'o' in a string are equal
+
+function:
+==========
+name: XO
+
+parameter str: string
+returns boolean
+
+psuedo code:
+================
+
+declare xXounter for x set to 0
+declare counter for o set to 0
+for each item in str:
+    if(item is an x ){
+        
+    }
+*/
