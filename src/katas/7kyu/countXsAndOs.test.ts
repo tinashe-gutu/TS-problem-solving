bcfb6101d2cd48e537d2b29af05aefcf76fe616d
Tinashe Gutu <tinashegutu2011@gmail.com>
2022-10-29 20:06:35 +0100

    fix misspecified assertions for countXsAndOs

--- src/katas/7kyu/countXsAndOs.test.ts
+++ src/katas/7kyu/countXsAndOs.test.ts
@@ -2,7 +2,7 @@ import countXO from './countXsAndOs'
 
 test('countXO check if number Xs and Os are equal', ()=>{
     expect(countXO('sdslxcxxlsdjo')).toBe(false)
-    expect('xxoo').toBe(true)
-    expect('sdsfd').toBe(false)
-    expect('OoXx').toBe(true)
+    expect(countXO('xxoo')).toBe(true)
+    expect(countXO('sdsfd')).toBe(true)
+    expect(countXO('OoXx')).toBe(true)
 })
